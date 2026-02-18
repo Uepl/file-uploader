92f58d371015ce4a7f1f5cd95182fc006d2c89aa
Uepl <khunanonk1@yahoo.com>
2026-02-19 00:40:17 +0700

    fix: no local

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -14,7 +14,7 @@ const handleLogin = async () => {
     isLoading.value = true;
 
     try {
-        const res = await fetch('http://localhost:3000/api/auth/login', {
+        const res = await fetch('/api/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email: email.value, password: password.value })
